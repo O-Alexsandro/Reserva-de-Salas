5200fc6d1e05fe0e70f0d8a3d97a2a6064baacbb
O-Alexsandro <alexsandrooliveiraribas@gmail.com>
2024-10-09 20:02:26 -0300

    Ajuste no Service de Reservations.

--- Reserva-de-Salas/src/main/java/com/alexsandro/Reserva/de/Salas/service/ReservationsService.java
+++ Reserva-de-Salas/src/main/java/com/alexsandro/Reserva/de/Salas/service/ReservationsService.java
@@ -1,8 +1,14 @@
 package com.alexsandro.Reserva.de.Salas.service;
 
+import com.alexsandro.Reserva.de.Salas.DTO.ReservationRequest;
 import com.alexsandro.Reserva.de.Salas.domain.reservas.Reservations;
+import com.alexsandro.Reserva.de.Salas.domain.salas.Rooms;
+import com.alexsandro.Reserva.de.Salas.domain.usuarios.Users;
 import com.alexsandro.Reserva.de.Salas.repository.ReservationsRepository;
+import com.alexsandro.Reserva.de.Salas.repository.RoomsRepository;
+import com.alexsandro.Reserva.de.Salas.repository.UsersRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -13,16 +19,35 @@ public class ReservationsService {
 
     private final ReservationsRepository reservationsRepository;
 
-    public ReservationsService(ReservationsRepository reservationsRepository) {
+    private final UsersRepository usersRepository;
+
+    private final RoomsRepository roomsRepository;
+
+    public ReservationsService(ReservationsRepository reservationsRepository, UsersRepository usersRepository, RoomsRepository roomsRepository) {
         this.reservationsRepository = reservationsRepository;
+        this.usersRepository = usersRepository;
+        this.roomsRepository = roomsRepository;
     }
 
     public List<Reservations> getReservation (){
         return reservationsRepository.findAll();
     }
 
-    public Reservations createReservations (Reservations reservations) {
-        return reservationsRepository.save(reservations);
+    public Reservations createReservations (ReservationRequest request) {
+        Users user = usersRepository.findById(request.userId()).orElseThrow(() -> new NoSuchElementException("User not found with id " + request.userId()));
+        Rooms rooms = roomsRepository.findById(request.roomId()).orElseThrow(() -> new NoSuchElementException("Room not found with id " + request.userId()));
+
+        Reservations newReservation = new Reservations();
+        newReservation.setUsers(user);
+        newReservation.setRooms(rooms);
+        newReservation.setDataReserve(request.dataReserve());
+        newReservation.setTimeStart(request.timeStart());
+        newReservation.setTimeEnd(request.timeEnd());
+        newReservation.setStatus(request.status());
+
+        System.out.println("Data Reserve: " + request.dataReserve());  // Log para verificar o valor
+
+        return reservationsRepository.save(newReservation);
     }
 
     public Reservations updateReservations (int id, Reservations reservations){
