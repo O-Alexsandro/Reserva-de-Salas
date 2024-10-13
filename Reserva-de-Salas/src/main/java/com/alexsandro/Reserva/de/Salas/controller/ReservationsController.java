bd37fb8aa5bc7267b7e671898f37cdae20c5d47a
O-Alexsandro <alexsandrooliveiraribas@gmail.com>
2024-10-13 15:03:44 -0300

    Implementação do Controller para o método de checagem de salas.

--- Reserva-de-Salas/src/main/java/com/alexsandro/Reserva/de/Salas/controller/ReservationsController.java
+++ Reserva-de-Salas/src/main/java/com/alexsandro/Reserva/de/Salas/controller/ReservationsController.java
@@ -1,7 +1,9 @@
 package com.alexsandro.Reserva.de.Salas.controller;
 
 import com.alexsandro.Reserva.de.Salas.DTO.ReservationRequest;
+import com.alexsandro.Reserva.de.Salas.DTO.RoomAvailabilityRequest;
 import com.alexsandro.Reserva.de.Salas.domain.reservas.Reservations;
+import com.alexsandro.Reserva.de.Salas.repository.ReservationsRepository;
 import com.alexsandro.Reserva.de.Salas.service.ReservationsService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @RestController
@@ -49,4 +52,11 @@ public class ReservationsController {
         Reservations deleteReservation = reservationsService.deleteReservations(id);
         return ResponseEntity.ok(deleteReservation);
     }
+
+    @PostMapping("/check")
+    public boolean isRoomAvailable (@RequestBody RoomAvailabilityRequest request){
+        boolean check = reservationsService.isRoomAvailable(request.roomId(), request.dataReserve(), request.timeStart(), request.timeEnd());
+        return check;
+    }
+
 }
